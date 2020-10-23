48bb0a6ffc4eec1234d768281882f9a51fc14c4d
Felipe Monteiro <felipedeamonteiro@gmail.com>
2020-10-23 14:23:37 -0300

    Add fake company repository to tests

---
+++ src/modules/companies/repositories/fakes/FakeCompaniesRepository.ts
@@ -0,0 +1,42 @@
+import { uuid } from 'uuidv4';
+
+import ICompanyRepository from '@modules/companies/repositories/ICompaniesRepository';
+import ICreateCompanyDTO from '@modules/companies/dtos/ICreateCompanyDTO';
+
+import Company from '../../infra/typeorm/entities/Company';
+
+class FakeCompanyRepository implements ICompanyRepository {
+  private companies: Company[] = [];
+
+  public async findById(id: string): Promise<Company | undefined> {
+    const findCompany = this.companies.find(company => company.id === id);
+
+    return findCompany;
+  }
+
+  public async findByEmail(email: string): Promise<Company | undefined> {
+    const findCompany = this.companies.find(company => company.email === email);
+
+    return findCompany;
+  }
+
+  public async createCompany(companyData: ICreateCompanyDTO): Promise<Company> {
+    const company = new Company();
+
+    Object.assign(company, { id: uuid() }, companyData);
+
+    return company;
+  }
+
+  public async saveCompany(company: Company): Promise<Company> {
+    const findIndex = this.companies.findIndex(
+      findCompany => findCompany.id === company.id,
+    );
+
+    this.companies[findIndex] = company;
+
+    return company;
+  }
+}
+
+export default FakeCompanyRepository;
